d7b17f9523c0c3f8bb9c2bcae338a50e3d85cd2a
Wojtek1990 <wojciechlizurej@wp.pl>
2023-01-08 17:36:02 +0100

    hotel added

--- src/App.js
+++ src/App.js
@@ -24,7 +24,15 @@ class App extends Component {
           rating: '8.8',
           description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
           image: ''
-        }
+        },
+        {
+          id: '3',
+          name: 'Nowy',
+          city: 'Warszawa',
+          rating: '8.9',
+          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+          image: ''
+        }        
       ]
     };
 
